174f9656d0f992d73427c2dcd6d9344a9c1e6deb
Nguyendat0819 <23010282@st.phenikaa-uni.edu.vn>
2026-08-21 21:50:33 +0700

    chưa dùng data

--- src/main/java/com/example/java_template/common/persistence/BaseRepository.java
+++ src/main/java/com/example/java_template/common/persistence/BaseRepository.java
@@ -1,9 +1,9 @@
-package com.example.java_template.common.persistence;
-import com.example.java_template.common.exception.ResourceNotFoundException;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BaseRepository<T,ID> extends JpaRepository<T,ID> {
-    default T findByIdOrThrow(ID id){
-        return findById(id).orElseThrow(() -> new ResourceNotFoundException(id));
-    }
-}
+//package com.example.java_template.common.persistence;
+//import com.example.java_template.common.exception.ResourceNotFoundException;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface BaseRepository<T,ID> extends JpaRepository<T,ID> {
+//    default T findByIdOrThrow(ID id){
+//        return findById(id).orElseThrow(() -> new ResourceNotFoundException(id));
+//    }
+//}
